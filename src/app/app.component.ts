1e4d64011519ee322482de6c1d33e0aa03ef2687
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2017-11-14 00:37:12 +0700

    Add input use foundation-datepicker in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+declare let $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  @ViewChild('datepicker1') public datepicker1: ElementRef;
+
+  constructor() {}
+
+  ngOnInit() {
+    $(this.datepicker1.nativeElement).fdatepicker({
+      initialDate: '31-12-2017',
+      format: 'dd-mm-yyyy',
+      disableDblClickSelection: true
+    });
+  }
 }
